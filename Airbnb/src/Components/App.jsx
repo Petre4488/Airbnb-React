cd4e5bac2c10186dfde007803c9e29fbc5df7584
Circeag <petrecirceag4488@gmail.com>
2023-04-23 23:58:52 +0300

    grid update

--- Airbnb/src/Components/App.jsx
+++ Airbnb/src/Components/App.jsx
@@ -2,13 +2,30 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Images from '../Images/Grid/index'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
+    <div className='App'> 
+      <div className='grid'> 
+        <img className="img1" src={Images.img1} />
+        <img className="img2" src={Images.img2} />
+        <img className="img3" src={Images.img3} />
+        <img className="img4" src={Images.img4} />
+        <img className="img5" src={Images.img5} />
+        <img className="img6" src={Images.img6} />
+        <img className="img7" src={Images.img7} />
+        <img className="img8" src={Images.img8} />
+        <img className="img9" src={Images.img9} />
+      </div>
+      <h1>Online Experiences</h1>
+      <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home</p>
+      <div className='Cards'>
+        <div className='Card-1'>
+          <img src={Images.card1}/>
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M512.5 190.4l-94.4 191.3-211.2 30.7 152.8 149-36.1 210.3 188.9-99.3 188.9 99.2-36.1-210.3 152.8-148.9-211.2-30.7z"></path><path d="M908.6 352.8l-253.9-36.9L541.2 85.8c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L370.3 315.9l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1L239 839.4a31.95 31.95 0 0 0 46.4 33.7l227.1-119.4 227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3zM665.3 561.3l36.1 210.3-188.9-99.2-188.9 99.3 36.1-210.3-152.8-149 211.2-30.7 94.4-191.3 94.4 191.3 211.2 30.7-152.8 148.9z"></path></svg>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ Airbnb/src/Images/Grid/index.js
@@ -0,0 +1,15 @@
+const Images =
+    {
+        img2: 'https://picsum.photos/200/300',
+        img1: 'https://picsum.photos/200/300',
+        img3: 'https://picsum.photos/200/300',
+        img4: 'https://picsum.photos/200/300',
+        img5: 'https://picsum.photos/200/300',
+        img6: 'https://picsum.photos/200/300',
+        img7: 'https://picsum.photos/200/300',
+        img8: 'https://loremflickr.com/320/240',
+        img9: 'https://loremflickr.com/320/240',
+        card1: 'https://loremflickr.com/320/240'
+    }
+
+export default Images
